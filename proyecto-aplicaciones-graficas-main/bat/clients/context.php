30e88fa91ffc5b264f8da9ba2b226f5e2767de9a
francis1359 <francisangefuentes18@gmail.com>
2023-05-31 22:36:14 -0600

    primer commit

---
+++ proyecto-aplicaciones-graficas-main/bat/clients/context.php
@@ -0,0 +1,21 @@
+<?php
+$servername = "violentos.mysql.database.azure.com"; // Nombre del servidor de MySQL
+$username = "Farmacia2023"; // Nombre de usuario de MySQL
+$password = "U-TW1gB02023"; // Contraseña de MySQL
+$database = "aplicacionesgraficas"; // Nombre de la base de datos
+
+try {
+    // Crear una conexión utilizando PDO
+    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Si la conexión se establece correctamente
+    //echo "Conexión exitosa a MySQL";
+} catch(PDOException $e) {
+    // En caso de error
+    //echo "Error de conexión: " . $e->getMessage();
+}
+
+// Cerrar la conexión
+//$conn = null;
+?>
